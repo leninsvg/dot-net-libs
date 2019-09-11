6fcb0475a114edf498f7e8650bd9ffe29c0281cd
Lenin samaniego <leninsvg@gmail.com>
2019-09-10 20:09:51 -0500

    no message

--- DotNetLibs/DotNetLibs.AzureBlobStorage/Models/UploadBlobModel.cs
+++ DotNetLibs/DotNetLibs.AzureBlobStorage/Models/UploadBlobModel.cs
@@ -6,6 +6,7 @@ namespace DotNetLibs.AzureBlobStorage.Models
 {
     public class UploadBlobModel
     {
+        public string ContainerName { get; set; }
         public string Name { get; set; }
         public byte[] Blob { get; set; }
     }
